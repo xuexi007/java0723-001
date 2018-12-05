1542f2f874de95e8232fec4f09d73154f4373ec0
sunny <hk109@126.com>
2018-12-05 10:51:27 +0800

    add hello.java

---
+++ hello.java
@@ -0,0 +1,22 @@
+package com.offcn.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.offcn.po.Mobile;
+//这是一个JAVA类
+public interface MobileService {
+	
+	@GET
+	@Path("/search/{number}")
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+   public Mobile search(@PathParam("number")String number);
+	
+	@GET
+	@Path("/search/{number}")
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+   public Mobile find(@PathParam("number")String number);
+}
